b0f29910ea759ce45cece91c67fa4c6c2dcc7aa5
Jon <jontiburzi@gmail.com>
2019-02-14 20:45:30 -0600

    Fresh start

--- src/scenes/Loading.js
+++ src/scenes/Loading.js
@@ -20,15 +20,7 @@ class Loading extends Phaser.Scene {
 
     	// We can still load assets in each scene, but I think it might be best to load the 
     	// heavy stuff here up front 
-    	this.load.image('logo', 'assets/logo.png');
     	this.load.image('block', 'assets/block.png');
-    	this.load.image('stone_tile', 'assets/stone_tile.png');
-        this.load.image('stone_tex_1', 'assets/stone_texture_01.png');
-        this.load.image('stone_tex_2', 'assets/stone_texture_02.png');
-        this.load.image('stone_tex_3', 'assets/stone_texture_03.png');
-        this.load.image('stone_tex_4', 'assets/stone_texture_04.png');
-        this.load.image('stone_tex_5', 'assets/stone_texture_05.png');
-        this.load.image('stone_tex_6', 'assets/stone_texture_06.png');
 
     	var loadingText = this.createText();
 
